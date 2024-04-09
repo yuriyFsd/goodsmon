e295f9d6287411a44f6799d5416e3093c2077071
alex <alexandr.orishchuk@ys.bet>
2024-04-09 10:29:12 +0300

    [mt-1] add array of goods processing

--- index.js
+++ index.js
@@ -1,83 +1,105 @@
 import fetch from 'node-fetch'
 //const fetch = require('node-fetch')
 import express from 'express'
+import cron from 'node-cron'
 const app = express()
 
 const port = 3000
-const outerUrl = 'https://google.com'
-const productOwnUrl = '/product/morozivo-90-g-svoa-linia-briket-mup'
+
+const goodsInitial = [
+    {url: '/product/morozivo-90-g-svoa-linia-briket-mup', titleSubstring: '<h1 class="page-title product-page__title">Морозиво 90 г Своя лінія Брикет м/уп</h1>'},
+    {url: '/product/morozivo-90g-lasunka-plombir-ice-cake-zi-smvanili-ta-z-kakao-u-pecivi-ta-kondglaz-z-arah', titleSubstring: '<h1 class="page-title product-page__title">Морозиво 90г Ласунка пломбір Ice-Cake зі см.ванілі та з какао у печиві та конд.глаз. з арах.</h1>'}
+]
 
 const vendorBaseUrl = 'https://www.atbmarket.com'
-const priceTopClass = 'class="product-price__top'
-const priceBottomClass = 'class="product-price__bottom'
-const productSubstring =
-    '<h1 class="page-title product-page__title">Морозиво 90 г Своя лінія Брикет м/уп</h1>'
+//const valueTopClass = 'class="product-value__top'
+//const valueBottomClass = 'class="product-value__bottom'
 
-async function getProductPageHtml() {
-    const response = await fetch(vendorBaseUrl + productOwnUrl) //, { method: 'GET', body: { x: 23 } }
-    //console.log({response})
+;(function () {
+    monitor()
+})()
+
+// cron.schedule('* * * * *', () => {
+//     logMessage()
+//     toRunByCrone()
+// })
+function logMessage() {
+    console.log('Cron job executed at:', new Date().toLocaleString())
+}
+
+async function monitor() {
+    const values = await Promise.all(
+        goodsInitial.map(({ url, titleSubstring }) => {
+            return loadProductValues(vendorBaseUrl + url, titleSubstring)
+        })
+    )
+    console.log({values})
+}
+
+async function getProductPageHtml(url) {
+    const response = await fetch(url)
     return await response.text()
 }
 
-app.get('/', async (req, res) => {
-    const htmlSource = await getProductPageHtml()
+async function loadProductValues(url, productSubstring) {
+    const htmlSource = await getProductPageHtml(url)
     const indexProductTitle = htmlSource.indexOf(productSubstring)
     const productOpenDivIndex = htmlSource.indexOf('<div', indexProductTitle)
     const productDivCloseIndex = findClosingTagIndex(htmlSource, productOpenDivIndex)
+    //console.log({indexProductTitle, productOpenDivIndex, productDivCloseIndex})
     const productHtmlBlock = htmlSource.substring(productOpenDivIndex, productDivCloseIndex)
-    const price = extractPrice(productHtmlBlock)
-    res.send(price) //`port ${port} is listening to you`)
+    return extractValues(productHtmlBlock)
+}
+
+app.get('/', async (req, res) => {
+    res.send(await loadProductValues)
 })
 
 app.listen(port, async () => {
-    const htmlSource = await getProductPageHtml()
-    const indexProductTitle = htmlSource.indexOf(productSubstring)
-    const productOpenDivIndex = htmlSource.indexOf('<div', indexProductTitle)
-    const productDivCloseIndex = findClosingTagIndex(htmlSource, productOpenDivIndex)
-    const productHtmlBlock = htmlSource.substring(productOpenDivIndex, productDivCloseIndex)
-    const price = extractPrice(productHtmlBlock)
-    //console.log(productHtmlBlock)
-    //const indexPriceTop = htmlSource.indexOf(priceTopClass)
-    //const indexPriceBottom = htmlSource.indexOf(priceBottomClass)
-    //console.log(productOpenDivIndex, indexPriceTop, indexPriceBottom)
-
-    console.log(port + ' is listening ok!!! ' + price )
+    console.log(port + ' is listening ok!!! ')
 })
-
 
 /**
  * @param {string} htmlString
  * @param {any} openDivIndex
- * @param {string} [tagName='div'] 
+ * @param {string} [tagName='div']
  */
 function findClosingTagIndex(htmlString, openDivIndex, tagName = 'div') {
-    let stack = [];
+    let stack = []
     const openTag = '<' + tagName
     const closeTag = `</${tagName}>`
     for (let i = openDivIndex; i < htmlString.length; i++) {
         if (htmlString[i] === '<') {
-        // Check if it's an opening div tag
+            // Check if it's an opening div tag
             if (htmlString.startsWith(openTag, i)) {
-                stack.push(openTag);
+                stack.push(openTag)
             } else if (htmlString.startsWith(closeTag, i)) {
                 // Check if it's a closing div tag
-                stack.pop();
+                stack.pop()
                 // If the stack is empty, we've found the matching closing tag
                 if (stack.length === 0) {
-                    return i;
+                    return i
                 }
             }
         }
     }
     // If no closing tag is found
-    return -1;
+    return -1
 }
 
 /**
  * @param {string} htmlString
  */
-function extractPrice(htmlString) {
-    const regex = /\d+\.|\d+/gm;
-    const matches = htmlString.match(regex);
-    return matches ? parseFloat(matches[0] + matches[1]) : null;
+function extractValues(htmlString) {
+    const matches = htmlString.match(/>\d+\.|>\d+/gm)
+    const values = []
+    matches?.reduce((prevVal, currentVal, index) => {
+        if(index % 2 === 1) {
+            values.push(parseFloat(prevVal.replace('>', '').concat(currentVal.replace('>', ''))) || null)
+        }
+        return currentVal
+    })
+    console.log(values)
+    return values
+    //return matches ? parseFloat(matches[0] + matches[1]) : null
 }
