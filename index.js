86bc3ee6cc34690aded79af6ab6f852fbbfd3b57
alex <alexandr.orishchuk@ys.bet>
2024-04-17 22:06:44 +0300

    [mt-1] update project structure

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-import fetch from 'node-fetch'
+//import fetch from 'node-fetch'
 //const fetch = require('node-fetch')
 import express from 'express'
 import cron from 'node-cron'
 const app = express()
-
+import { GoodsExtractionService } from './services/goods.extraction.service.js'
+//import { server } from './api/index.js'
+import testRoute from './api/routes/testRoute.js'
+app.use('/flags', express.static('public'))
+app.use('/test1', testRoute)
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('test error')
+})
 const port = 3000
 
 const goodsInitial = [
-    {url: '/product/morozivo-90-g-svoa-linia-briket-mup', titleSubstring: '<h1 class="page-title product-page__title">Морозиво 90 г Своя лінія Брикет м/уп</h1>'},
-    {url: '/product/morozivo-90g-lasunka-plombir-ice-cake-zi-smvanili-ta-z-kakao-u-pecivi-ta-kondglaz-z-arah', titleSubstring: '<h1 class="page-title product-page__title">Морозиво 90г Ласунка пломбір Ice-Cake зі см.ванілі та з какао у печиві та конд.глаз. з арах.</h1>'}
+    {
+        url: 'https://www.atbmarket.com/product/morozivo-90-g-svoa-linia-briket-mup',
+        titleOpenTag: '<h1 class="page-title product-page__title">',
+        title: 'Морозиво 90 г Своя лінія Брикет м/уп'
+    },
+    {
+        url: 'https://www.atbmarket.com/product/morozivo-90g-lasunka-plombir-ice-cake-zi-smvanili-ta-z-kakao-u-pecivi-ta-kondglaz-z-arah',
+        titleOpenTag: '<h1 class="page-title product-page__title">',
+        title: 'Морозиво 90г Ласунка пломбір Ice-Cake зі см.ванілі та з какао у печиві та конд.глаз. з арах.'
+    },
+    {
+        url: 'https://www.atbmarket.com/product/morozivo-100-g-oliver-smith-lakomij-plombir-u-zbitij-konditerskij-glazuri',
+        titleOpenTag: '<h1 class="page-title product-page__title">',
+        title: 'Морозиво 100  г Oliver Smith Лакомий пломбір у збитій кондитерській глазурі'
+    }
 ]
 
-const vendorBaseUrl = 'https://www.atbmarket.com'
-//const valueTopClass = 'class="product-value__top'
-//const valueBottomClass = 'class="product-value__bottom'
-
-;(function () {
-    monitor()
-})()
+monitor().catch((error) => console.log(error))
 
 // cron.schedule('* * * * *', () => {
 //     logMessage()
@@ -28,78 +43,24 @@ function logMessage() {
 }
 
 async function monitor() {
+    const goodsExtractionService = new GoodsExtractionService()
     const values = await Promise.all(
-        goodsInitial.map(({ url, titleSubstring }) => {
-            return loadProductValues(vendorBaseUrl + url, titleSubstring)
+        goodsInitial.map(({ url, titleOpenTag, title }) => {
+            return goodsExtractionService.loadProductValues(url, titleOpenTag.concat(title))
         })
     )
-    console.log({values})
+    let index = 0
+    const updatedGoods = goodsInitial.map(({ url, title }) => {
+        return `${url}  : ${values[index++]}`
+    })
+    console.log({ updatedGoods })
+    return updatedGoods
 }
 
-async function getProductPageHtml(url) {
-    const response = await fetch(url)
-    return await response.text()
-}
-
-async function loadProductValues(url, productSubstring) {
-    const htmlSource = await getProductPageHtml(url)
-    const indexProductTitle = htmlSource.indexOf(productSubstring)
-    const productOpenDivIndex = htmlSource.indexOf('<div', indexProductTitle)
-    const productDivCloseIndex = findClosingTagIndex(htmlSource, productOpenDivIndex)
-    //console.log({indexProductTitle, productOpenDivIndex, productDivCloseIndex})
-    const productHtmlBlock = htmlSource.substring(productOpenDivIndex, productDivCloseIndex)
-    return extractValues(productHtmlBlock)
-}
-
-app.get('/', async (req, res) => {
-    res.send(await loadProductValues)
-})
+// app.get('/', async (req, res) => {
+//     res.send(await monitor())
+// })
 
 app.listen(port, async () => {
     console.log(port + ' is listening ok!!! ')
 })
-
-/**
- * @param {string} htmlString
- * @param {any} openDivIndex
- * @param {string} [tagName='div']
- */
-function findClosingTagIndex(htmlString, openDivIndex, tagName = 'div') {
-    let stack = []
-    const openTag = '<' + tagName
-    const closeTag = `</${tagName}>`
-    for (let i = openDivIndex; i < htmlString.length; i++) {
-        if (htmlString[i] === '<') {
-            // Check if it's an opening div tag
-            if (htmlString.startsWith(openTag, i)) {
-                stack.push(openTag)
-            } else if (htmlString.startsWith(closeTag, i)) {
-                // Check if it's a closing div tag
-                stack.pop()
-                // If the stack is empty, we've found the matching closing tag
-                if (stack.length === 0) {
-                    return i
-                }
-            }
-        }
-    }
-    // If no closing tag is found
-    return -1
-}
-
-/**
- * @param {string} htmlString
- */
-function extractValues(htmlString) {
-    const matches = htmlString.match(/>\d+\.|>\d+/gm)
-    const values = []
-    matches?.reduce((prevVal, currentVal, index) => {
-        if(index % 2 === 1) {
-            values.push(parseFloat(prevVal.replace('>', '').concat(currentVal.replace('>', ''))) || null)
-        }
-        return currentVal
-    })
-    console.log(values)
-    return values
-    //return matches ? parseFloat(matches[0] + matches[1]) : null
-}
