ecc380a27fce42c84afeb1ee7a6016c1a726f4f9
Yuri <yuriy1@yahoo.com>
2024-05-22 22:13:52 +0300

    [mt-1] downgrade to older node

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 //import fetch from 'node-fetch'
 //const fetch = require('node-fetch')
-import express from 'express'
-import cron from 'node-cron'
+const express = require('express')
+const cron = require ('node-cron')
 const app = express()
-import { GoodsExtractionService } from './services/goods.extraction.service.js'
+const { GoodsExtractionService } = require('./services/goods.extraction.service.js')
 //import { server } from './api/index.js'
-import testRoute from './api/routes/testRoute.js'
-import telegram from './services/telegram.js'
+const testRoute = require('./api/routes/testRoute.js')
+const telegram = require('./services/telegram.js')
 app.use('/flags', express.static('public'))
 app.use('/test1', testRoute)
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('test error')
+  console.error(err.stack)
+  res.status(500).send('test error')
 })
 const port = 3000
 
 const goodsInitial = [
-    {
-        url: 'https://www.atbmarket.com/product/morozivo-90-g-svoa-linia-briket-mup',
-        titleOpenTag: '<h1 class="page-title product-page__title">',
-        title: 'Морозиво 90 г Своя лінія Брикет м/уп'
-    },
-    {
-        url: 'https://www.atbmarket.com/product/morozivo-90g-lasunka-plombir-ice-cake-zi-smvanili-ta-z-kakao-u-pecivi-ta-kondglaz-z-arah',
-        titleOpenTag: '<h1 class="page-title product-page__title">',
-        title: 'Морозиво 90г Ласунка пломбір Ice-Cake зі см.ванілі та з какао у печиві та конд.глаз. з арах.'
-    },
-    {
-        url: 'https://www.atbmarket.com/product/morozivo-100-g-oliver-smith-lakomij-plombir-u-zbitij-konditerskij-glazuri',
-        titleOpenTag: '<h1 class="page-title product-page__title">',
-        title: 'Морозиво 100  г Oliver Smith Лакомий пломбір у збитій кондитерській глазурі'
-    },
-    {
-        url: 'https://www.atbmarket.com/product/ikra-310-g-veres-z-kabackiv-ekstra-skbanka',
-        titleOpenTag: '<h1 class="page-title product-page__title">',
-        title: 'Ікра 310 г Верес з кабачків Екстра ск/банка'
-    },
+  {
+    url: 'https://www.atbmarket.com/product/morozivo-90-g-svoa-linia-briket-mup',
+    titleOpenTag: '<h1 class="page-title product-page__title">',
+    title: 'Морозиво 90 г Своя лінія Брикет м/уп'
+  },
+  {
+    url: 'https://www.atbmarket.com/product/morozivo-90g-lasunka-plombir-ice-cake-zi-smvanili-ta-z-kakao-u-pecivi-ta-kondglaz-z-arah',
+    titleOpenTag: '<h1 class="page-title product-page__title">',
+    title:
+      'Морозиво 90г Ласунка пломбір Ice-Cake зі см.ванілі та з какао у печиві та конд.глаз. з арах.'
+  },
+  {
+    url: 'https://www.atbmarket.com/product/morozivo-100-g-oliver-smith-lakomij-plombir-u-zbitij-konditerskij-glazuri',
+    titleOpenTag: '<h1 class="page-title product-page__title">',
+    title: 'Морозиво 100  г Oliver Smith Лакомий пломбір у збитій кондитерській глазурі'
+  },
+  {
+    url: 'https://www.atbmarket.com/product/ikra-310-g-veres-z-kabackiv-ekstra-skbanka',
+    titleOpenTag: '<h1 class="page-title product-page__title">',
+    title: 'Ікра 310 г Верес з кабачків Екстра ск/банка'
+  }
 ]
 
 monitor().catch((error) => console.log(error))
@@ -45,27 +46,27 @@ monitor().catch((error) => console.log(error))
 //     toRunByCrone()
 // })
 function logMessage() {
-    console.log('Cron job executed at:', new Date().toLocaleString())
+  console.log('Cron job executed at:', new Date().toLocaleString())
 }
 
 async function monitor() {
-    const goodsExtractionService = new GoodsExtractionService()
-    const values = await Promise.all(
-        goodsInitial.map(({ url, titleOpenTag, title }) => {
-            return goodsExtractionService.loadProductValues(url, titleOpenTag.concat(title))
-        })
-    )
-    let index = 0
-    const updatedGoods = goodsInitial.map(({ url, title }) => {
-        const goodInfo = `${url}  : ${values[index]}`
-        if (values[index]?.length > 1) {
-           telegram.log(goodInfo)
-        }
-        index++
-        return goodInfo
+  const goodsExtractionService = new GoodsExtractionService()
+  const values = await Promise.all(
+    goodsInitial.map(({ url, titleOpenTag, title }) => {
+      return goodsExtractionService.loadProductValues(url, titleOpenTag.concat(title))
     })
-    console.log({ updatedGoods })
-    return updatedGoods
+  )
+  let index = 0
+  const updatedGoods = goodsInitial.map(({ url, title }) => {
+    const goodInfo = `${url}  : ${values[index]}`
+    if (values[index]?.length > 1) {
+      telegram.log(goodInfo)
+    }
+    index++
+    return goodInfo
+  })
+  console.log({ updatedGoods })
+  return updatedGoods
 }
 
 // app.get('/', async (req, res) => {
@@ -73,5 +74,5 @@ async function monitor() {
 // })
 
 app.listen(port, async () => {
-    console.log(port + ' is listening ok!!! ')
+  console.log(port + ' is listening ok!!! ')
 })
