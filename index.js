e51d879089351f8f4692f574100405ec5f14ca7d
Yuri <yuriy1@yahoo.com>
2024-06-26 18:01:36 +0300

    [mt-1] add cron run and update alerts

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('test error')
 })
-const port = 3000
+const port = process.env.PORT || 4000;
+const runTimeRange = process.env.APP_TIME_RANGE || '*/10 * * * *'
 
 const goodsInitial = [
     {
@@ -40,10 +41,10 @@ const goodsInitial = [
 
 monitor().catch((error) => console.log(error))
 
-// cron.schedule('* * * * *', () => {
-//     logMessage()
-//     toRunByCrone()
-// })
+cron.schedule(runTimeRange, () => {
+    logMessage()
+    monitor()
+})
 function logMessage() {
     console.log('Cron job executed at:', new Date().toLocaleString())
 }
@@ -58,8 +59,8 @@ async function monitor() {
     let index = 0
     const updatedGoods = goodsInitial.map(({ url, title }) => {
         const goodInfo = `${url}  : ${values[index]}`
-        if (values[index]?.length > 1) {
-           telegram.log(goodInfo)
+        if (typeof values[index] === 'object' && values[index]?.length > 1) {
+            telegram.log(goodInfo)
         }
         index++
         return goodInfo
